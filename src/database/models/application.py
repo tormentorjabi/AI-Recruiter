1c7f1345efa4e701853b256335bc82ce483aabac
Maxim Kuznetsov <melixmilan@gmail.com>
2025-03-31 20:49:19 +0500

    KAN-19 application model description

--- src/database/models/application.py
+++ src/database/models/application.py
@@ -9,6 +9,14 @@ from session import Base
 
 
 class Application(Base):
+    """
+    Модель отклика на вакансию
+    
+    Fields:
+        candidate_id (int): FK на соискателя по вакансии
+        vacancy_id (str): ID вакансии
+        application_date (datetime): Дата получения отклика
+    """
     __tablename__ = 'applications'
     
     id = Column(Integer, primary_key=True)
