24326dc1c3fb8a8e74544f19664028abe00fc0de
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-01 12:04:35 +0500

    KAN-19 desired_position model implemented

---
+++ src/database/models/desired_position.py
@@ -0,0 +1,26 @@
+from sqlalchemy import (
+    Column,
+    Integer, 
+    String,
+    Numeric,
+    ForeignKey
+)
+from session import Base
+
+
+class DesiredPosition(Base):
+    """
+    Модель желаемых условий труда кандидата
+    
+    Fields:
+        resume_id (int): FK на резюме кандидата
+        position (str): Желаемая позиция
+        salary (numeric): Желаемая заработная плата
+    """
+    __tablename__ = 'desired_positions'
+    
+    id = Column(Integer, primary_key=True)
+    resume_id = Column(Integer, ForeignKey('resumes.id'))
+    position = Column(String(255))
+    salary = Column(Numeric)
+    
