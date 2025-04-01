e2cc4b55856b61559108bd0f27ab390149ebc4cf
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-01 12:02:59 +0500

    KAN-19 desired_position_schedule model implemented

---
+++ src/database/models/desired_position_schedule.py
@@ -0,0 +1,22 @@
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer
+)
+from session import Base
+
+
+class DesiredPositionSchedule(Base):
+    """
+    Модель обёртки связи M:M желаемых условий труда к графику работы
+    
+    Fields:
+        schedule_id (int): FK на график работы
+        desired_position_id (int): FK на желаемые условия труда
+    """
+    __tablename__ = 'desired_position_schedule'
+    
+    id = Column(Integer, primary_key=True)
+    desired_position_id = Column(Integer, ForeignKey('desired_positions.id'))
+    schedule_id = Column(Integer, ForeignKey('work_schedules.id'))
+    
