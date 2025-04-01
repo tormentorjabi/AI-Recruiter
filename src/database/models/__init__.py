005fa17817702dbe7f90eedc2816ef3f7c04878b
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-01 13:22:33 +0500

    KAN-19 import models

---
+++ src/database/models/__init__.py
@@ -0,0 +1,38 @@
+from .analysis_result import AnalysisResult
+from .application import Application
+from .bot_interaction import BotInteraction
+from .bot_question import BotQuestion
+from .candidate_answer import CandidateAnswer
+from .candidate_skill import CandidateSkill
+from .candidate import Candidate
+from .desired_position import DesiredPosition
+from .desired_position_employment import DesiredPositionEmployment
+from .desired_position_schedule import DesiredPositionSchedule
+from .education import Education
+from .employment_type import EmploymentType
+from .hr_notification import HrNotification
+from .resume import Resume
+from .skill import Skill
+from .work_experience import WorkExperience
+from .work_schedule import WorkSchedule
+
+
+__all__ = [
+    'AnalysisResult',
+    'Application',
+    'BotInteraction',
+    'BotQuestion',
+    'CandidateAnswer',
+    'CandidateSkill',
+    'Candidate',
+    'DesiredPosition',
+    'DesiredPositionEmployment',
+    'DesiredPositionSchedule',
+    'Education',
+    'EmploymentType',
+    'HrNotification',
+    'Resume',
+    'Skill',
+    'WorkExperience',
+    'WorkSchedule'
+]
