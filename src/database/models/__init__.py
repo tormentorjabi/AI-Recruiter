943d68c9f5a936a3b2851255e72b498d031a1714
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-01 14:07:38 +0500

    KAN-19 import models fix

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,38 +1,38 @@
-from .analysis_result import AnalysisResult
-from .application import Application
-from .bot_interaction import BotInteraction
-from .bot_question import BotQuestion
-from .candidate_answer import CandidateAnswer
-from .candidate_skill import CandidateSkill
 from .candidate import Candidate
-from .desired_position import DesiredPosition
-from .desired_position_employment import DesiredPositionEmployment
-from .desired_position_schedule import DesiredPositionSchedule
-from .education import Education
-from .employment_type import EmploymentType
-from .hr_notification import HrNotification
 from .resume import Resume
+from .work_schedule import WorkSchedule
+from .desired_position import DesiredPosition
+from .employment_type import EmploymentType
 from .skill import Skill
 from .work_experience import WorkExperience
-from .work_schedule import WorkSchedule
+from .education import Education
+from .application import Application
+from .analysis_result import AnalysisResult
+from .hr_notification import HrNotification
+from .bot_interaction import BotInteraction
+from .candidate_answer import CandidateAnswer
+from .bot_question import BotQuestion
+from .desired_position_employment import DesiredPositionEmployment
+from .desired_position_schedule import DesiredPositionSchedule
+from .candidate_skill import CandidateSkill
 
 
 __all__ = [
-    'AnalysisResult',
-    'Application',
-    'BotInteraction',
-    'BotQuestion',
-    'CandidateAnswer',
-    'CandidateSkill',
-    'Candidate',
-    'DesiredPosition',
-    'DesiredPositionEmployment',
-    'DesiredPositionSchedule',
-    'Education',
-    'EmploymentType',
-    'HrNotification',
-    'Resume',
-    'Skill',
-    'WorkExperience',
-    'WorkSchedule'
+    "Candidate",
+    "Resume",
+    "WorkSchedule",
+    "DesiredPosition",
+    "EmploymentType",
+    "Skill",
+    "WorkExperience",
+    "Education",
+    "Application",
+    "AnalysisResult",
+    "HrNotification",
+    "BotInteraction",
+    "CandidateAnswer",
+    "BotQuestion",
+    "DesiredPositionEmployment",
+    "DesiredPositionSchedule",
+    "CandidateSkill",
 ]
