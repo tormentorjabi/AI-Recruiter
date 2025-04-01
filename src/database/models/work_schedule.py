a62aeaf0d71652e288fe339c13a266d5ba80a243
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-01 12:13:16 +0500

    KAN-19 work_schedule model implemented

---
+++ src/database/models/work_schedule.py
@@ -0,0 +1,26 @@
+from sqlalchemy import (
+    Column,
+    Integer, 
+    String
+)
+from session import Base
+
+
+class WorkSchedule(Base):
+    """
+    Модель графика работы
+    
+    Fields:
+        schedule (str): График работы (
+            'Полный день',
+            'Сменный график',
+            'Гибкий график',
+            'Удаленная работа',
+            'Вахтовый метод'
+        )
+    """
+    __tablename__ = 'work_schedules'
+    
+    id = Column(Integer, primary_key=True)
+    schedule = Column(String(50))
+    
