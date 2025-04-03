3d92a20e46835e318c4540fc70d342da46dae1b4
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-03 16:16:24 +0500

    convert main app lifecycle to async version

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
+import asyncio
 
 from dotenv import load_dotenv
+from bot.core.bot import bot, dp
 from gigachat_module.client import get_gigachat_client
 from langchain_core.messages import HumanMessage, SystemMessage
 
@@ -33,10 +35,26 @@ def custom_llm_task(prompt: str) -> str:
         return None
     
 
-while(True):
-    user_msg = input("ПРОМПТ ПОЛЬЗОВАТЕЛЯ: ")
-    if user_msg == "СТОП":
-        break
-    response = custom_llm_task(user_msg)
-    print(response)
+async def custom_llm_task_loop():
+    while True:
+        user_input = await asyncio.to_thread(input, "ПРОМПТ ПОЛЬЗОВАТЕЛЯ: ")
+        if user_input.strip().upper() == "СТОП":
+            break
+        response = await asyncio.to_thread(custom_llm_task, user_input)
+        print(f"GigaChat ответ: {response}")
+
+
+async def main() -> None:
+    bot_task = asyncio.create_task(dp.start_polling(bot))
+    llm_task = asyncio.create_task(custom_llm_task_loop())
+
+    await asyncio.gather(
+        bot_task,
+        llm_task
+    )
     
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Turning off")
