e8c1b1cb960bfecb767f7db52882309cbfb1b7f3
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-03 20:13:24 +0500

    include routers for new commands

--- src/bot/core/bot.py
+++ src/bot/core/bot.py
@@ -1,10 +1,15 @@
 from aiogram.fsm.storage.memory import MemoryStorage
-from src.bot.config import TELEGRAM_BOT_TOKEN
 from aiogram import Bot, Dispatcher
+
+from src.bot.config import TELEGRAM_BOT_TOKEN
 from src.bot.handlers.admin_commands import admin_router
+from src.bot.handlers.hr_registration import registration_router
 
 
 memory = MemoryStorage()
 bot = Bot(TELEGRAM_BOT_TOKEN)
 dp = Dispatcher(storage=memory)
-dp.include_router(admin_router)
+dp.include_routers(
+    admin_router,
+    registration_router
+)
