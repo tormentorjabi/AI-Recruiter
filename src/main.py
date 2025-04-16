c24b56ed00b370cc4db9930dc80357138d586990
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-16 21:28:20 +0500

    KAN-52 added list of commands and logger

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import os
 import asyncio
+import logging
 
+from aiogram.types import BotCommand
 from dotenv import load_dotenv
 from src.bot.core.bot import bot, dp
 from src.gigachat_module.client import get_gigachat_client
 from langchain_core.messages import HumanMessage, SystemMessage
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 load_dotenv()
 
@@ -45,16 +50,34 @@ async def custom_llm_task_loop():
 
 
 async def main() -> None:
-    bot_task = asyncio.create_task(dp.start_polling(bot))
-    llm_task = asyncio.create_task(custom_llm_task_loop())
+    try:
+        commands = [
+            BotCommand(command='/start', description='Запуск бота'),
+            BotCommand(command='/register_hr', description='Регистрация (для HR-специалиста)'),
+            BotCommand(command='/change_work_mode', description='Сменить режим работы (для HR-специалиста)'),
+            BotCommand(command='/delete_hr', description='Удаление HR-специалиста (для Админа)'),
+            BotCommand(command='/generate_token', description='Генерация токена регистрации (для Админа)'),
+            BotCommand(command='/list_hr', description='Список зарегистрированых HR-специалистов (для Админа)')
+        ]
+        
+        set_bot_commands = asyncio.create_task(bot.set_my_commands(commands=commands))
+        bot_task = asyncio.create_task(dp.start_polling(bot))
+        llm_task = asyncio.create_task(custom_llm_task_loop())
 
-    await asyncio.gather(
-        bot_task,
-        llm_task
-    )
+        await asyncio.gather(
+            set_bot_commands,
+            bot_task,
+            llm_task
+        )
+    except Exception as e:
+        logger.error(f'Error occured: {e}', exc_info=True)
+    finally:
+        await bot.session.close()
+        logger.info('Bot session closed.')
     
 if __name__ == "__main__":
     try:
+        print('Power on...')
         asyncio.run(main())
-    except KeyboardInterrupt:
-        print("Turning off")
+    except (KeyboardInterrupt, SystemExit):
+        print("Turning off...")
