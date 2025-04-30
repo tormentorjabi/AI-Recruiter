c6845f90a42db2ad4075ea8cee77e7a33ead7475
Maxim Kuznetsov <melixmilan@gmail.com>
2025-04-30 18:07:52 +0500

    KAN-66: parsing tasks test invocation implemented

--- src/gigachat_module/utils/resume_processing.py
+++ src/gigachat_module/utils/resume_processing.py
@@ -1,10 +1,46 @@
 import logging
 import asyncio
 
-from src.gigachat_module.parser import get_multiple_resumes
+from typing import Tuple, List
+from datetime import timezone, datetime
 
+from src.gigachat_module.parser import parse_multiple_resumes, parse_resume
+from src.database.utils.entry_creation import create_candidates_entries
+
+from src.database.session import Session
+from src.database.models import Vacancy
 
 logger = logging.getLogger(__name__)
+
+
+def get_resumes_data() -> List[Tuple[str, int]]:
+    '''
+        TODO:
+            - Возвращать список URL+вакансия
+    '''
+    with Session() as db:
+        try:
+            vacancy = Vacancy(
+                title="Тестовый прогон",
+                description="Описание вакансии",
+                created_at=datetime.now(timezone.utc)
+            )
+            db.add(vacancy)
+            db.flush()
+            
+            vacancy_id = vacancy.id
+            
+            db.commit()
+        except Exception as e:
+            logger.error(f"COULDNT CREATE VACANCY: {str(e)}")
+            return
+    
+    test_return_data = [
+        ("https://nizhny-tagil.hh.ru/resume/27597eb2ff0e7799050039ed1f494d63716948", vacancy_id),
+        ("https://ekaterinburg.hh.ru/resume/0343600aff0c1b1b130039ed1f4a7a6e7a494c", vacancy_id) 
+    ]
+    
+    return test_return_data
 
 
 async def resume_processing_task(delay_hours: int = 24) -> None:
@@ -16,13 +52,10 @@ async def resume_processing_task(delay_hours: int = 24) -> None:
                         вообще весь проект без API HH не работает, так как иначе ссылки на резюме нужно ручками
                         кидать HR'у, что как бы хрень какая-то.
             '''
-            url_list = [
-                "https://ekaterinburg.hh.ru/resume/0343600aff0c1b1b130039ed1f4a7a6e7a494c",
-            ]
-            results = await get_multiple_resumes(url_list)
-            
-            # Заглушка, проверить результати парсинга
-            logger.warning(results)
+            resumes_data = get_resumes_data()              
+            parsed_results = await parse_multiple_resumes(resumes_data=resumes_data)
+            await create_candidates_entries(resumes=parsed_results)
+                
         except Exception as e:
             logger.error(f'Error in resume_processing_task: {str(e)}', exc_info=True)
             
--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 from src.bot.core.bot import bot, dp
 from src.bot.utils.check_abandoned_forms import check_abandoned_forms
-from src.gigachat_module.parser import get_multiple_resumes
+from src.gigachat_module.parser import parse_multiple_resumes
 from src.gigachat_module.utils.resume_processing import resume_processing_task
 # [DEV MODE ONLY]
 # from src.gigachat_module.custom_llm_task import custom_llm_task_loop
@@ -44,18 +44,18 @@ async def main() -> None:
                 - check_abandoned_forms: проверка брошенных анкет и отправка напоминаний.
                 - direct_prompts_to_gigachat: прямое общение с моделью GigaChat [DEV MODE ONLY]
         ''' 
-        resume_processing = asyncio.create_task(resume_processing_task(delay_hours=1))
-        set_bot_commands = asyncio.create_task(bot.set_my_commands(commands=commands))
-        bot_task = asyncio.create_task(dp.start_polling(bot))
-        abandoned_forms_checks = asyncio.create_task(check_abandoned_forms(bot=bot, delay_minutes=30))
+        resume_processing = asyncio.create_task(resume_processing_task(delay_hours=24))
+        #set_bot_commands = asyncio.create_task(bot.set_my_commands(commands=commands))
+        #bot_task = asyncio.create_task(dp.start_polling(bot))
+        #abandoned_forms_checks = asyncio.create_task(check_abandoned_forms(bot=bot, delay_minutes=30))
         # [DEV MODE ONLY]
         # direct_prompts_to_gigachat = asyncio.create_task(custom_llm_task_loop()) 
 
         await asyncio.gather(
             resume_processing,
-            set_bot_commands,
-            bot_task,
-            abandoned_forms_checks,
+            #set_bot_commands,
+            #bot_task,
+            #abandoned_forms_checks,
             # [DEV MODE ONLY]
             # direct_prompts_to_gigachat
         )
