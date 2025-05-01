f4402fba28b95fd3bbdde0c3f194bf3e9a0fe5f7
Maxim Kuznetsov <melixmilan@gmail.com>
2025-05-01 17:08:46 +0500

    minor: code refactoring

--- src/database/utils/entry_update.py
+++ src/database/utils/entry_update.py
@@ -1,5 +1,6 @@
 import logging
 
+from typing import Optional
 from datetime import datetime, timezone
 
 from src.database.session import Session
@@ -9,18 +10,22 @@ from src.database.models import Resume
 logger = logging.getLogger(__name__)
 
 
-async def update_candidate_entry_resume_score(resume_id: int, score: int) -> None:
+async def update_candidate_entry_resume_score(resume_id: int, score: int) -> Optional[None]:
     '''Добавить оценку по скринингу резюме в сущность кандидата'''
     try:
         with Session() as db:
             resume = db.query(Resume).get(resume_id)
-            if resume:
-                resume.gigachat_score = score
-                resume.analysis_status = 'completed'
-                resume.update_at = datetime.now(timezone.utc)
-                db.commit()
+            
+            if not resume:
+                logger.warning(f'No resume with ID: {resume_id}')
                 return
-            return
+            
+            resume.gigachat_score = score
+            resume.analysis_status = 'completed'
+            resume.update_at = datetime.now(timezone.utc)
+            
+            db.commit()
+
     except Exception as e:
         logger.error(f'Failed to update resume score for resume_id: {resume_id}. {str(e)}')
         return
