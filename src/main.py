516f1ba8a7796a142a3f37b7b1c22f9ff6b669cb
Maxim Kuznetsov <melixmilan@gmail.com>
2025-05-05 18:02:50 +0500

    core: BotCommands rearranged

--- src/main.py
+++ src/main.py
@@ -21,15 +21,15 @@ async def main() -> None:
             # Production commands
             BotCommand(command='/start', description='Запуск бота'),
             BotCommand(command='/cancel', description='Отменить процесс заполнение анкеты (для соискателя по вакансии)'),
+            BotCommand(command='/get_reviews', description='Открыть панель HR (для HR-специалиста)'),
             BotCommand(command='/register_hr', description='Регистрация (для HR-специалиста)'),
-            BotCommand(command='/get_reviews', description='Посмотреть решения по кандидатам (для HR-специалиста)'),
             BotCommand(command='/generate_token', description='Генерация токена регистрации (для Админа)'),
             BotCommand(command='/delete_hr', description='Удаление HR-специалиста (для Админа)'),
             BotCommand(command='/list_hr', description='Список зарегистрированых HR-специалистов (для Админа)'),
              # [DEV MODE ONLY] commands
             BotCommand(command='/clr_db', description='Очистить БД (DEV MODE ONLY)'),
-            # BotCommand(command='/token_test', description='Тест: Регистрация клиента по токену + анкета (DEV MODE ONLY)'),
-            # BotCommand(command='/notification_test', description='Тест: Меню с решениями для HR (DEV MODE ONLY)'),
+            BotCommand(command='/token_test', description='Тест: Регистрация клиента по токену + анкета (DEV MODE ONLY)'),
+            BotCommand(command='/notification_test', description='Тест: Меню с решениями для HR (DEV MODE ONLY)'),
         ]
         
         '''
@@ -40,13 +40,13 @@ async def main() -> None:
                 - check_abandoned_forms: проверка брошенных анкет и отправка напоминаний.
                 - direct_prompts_to_gigachat: прямое общение с моделью GigaChat [DEV MODE ONLY]
         ''' 
-        resumes_processing = asyncio.create_task(resumes_processing_task(delay_hours=24))
+        #resumes_processing = asyncio.create_task(resumes_processing_task(delay_hours=24))
         set_bot_commands = asyncio.create_task(bot.set_my_commands(commands=commands))
         bot_task = asyncio.create_task(dp.start_polling(bot))
         abandoned_forms_checks = asyncio.create_task(check_abandoned_forms(bot=bot, delay_minutes=30))
 
         await asyncio.gather(
-            resumes_processing,
+            #resumes_processing,
             set_bot_commands,
             bot_task,
             abandoned_forms_checks,
