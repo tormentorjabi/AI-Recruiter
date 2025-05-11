67f8eaeb446e4108320e15c33f9241b92b75382a
Maxim Kuznetsov <melixmilan@gmail.com>
2025-05-11 18:14:53 +0500

    feature: modular `Telegram screening`

--- src/gigachat_module/telegram_screening.py
+++ src/gigachat_module/telegram_screening.py
@@ -7,14 +7,10 @@ from langchain_core.messages import SystemMessage, HumanMessage
 from .client import get_gigachat_client
 from src.gigachat_module.utils.formatters import candidate_answers_formatter
 
-logger = logging.getLogger(__name__)
+from src.database.session import Session
+from src.database.models import BotQuestion
 
-# Ключи для поиска вопросов должны строго совпадать с вопросами по вакансии.
-# Данные для этих ключей взяты из списка текущих вопросов по вакансии из: tests/bot_questions_data.py: QUESTION_DATA
-CUSTOMER_FOCUS_QUESTION_KEY = 'Если ответили "Да" на предыдущий вопрос: по каким признакам вы считаете себя клиентоориентированным? Приведите пример ситуации, когда вы проявили клиентоориентированность'
-SOFTWARE_QUESTION_KEY = 'Какими офисными программами вы владеете?'
-STRESS_QUESTION_KEY_PREFIX = 'Согласны ли вы, что работа в контактном центре может быть стрессовой?'
-STRESS_QUESTION_KEY = 'Почему вы так считаете?'
+logger = logging.getLogger(__name__)
 
 
 class TelegramScreening:
@@ -56,30 +52,34 @@ class TelegramScreening:
             criteria_text += f"- {key.replace('_', ' ').title()}: {value}\n"
         return criteria_text
     
-    def _collect_tasks(self, candidate_responses_json: str) -> List[HumanMessage]:
+    def _collect_tasks(
+        self, 
+        candidate_responses_json: str,
+        vacancy_id: int,
+    ) -> List[HumanMessage]:
         tasks = []
-        focus_key = [CUSTOMER_FOCUS_QUESTION_KEY]
-        software_key = [SOFTWARE_QUESTION_KEY]
-        stress_key = [STRESS_QUESTION_KEY_PREFIX, STRESS_QUESTION_KEY]
 
         try:
-            focus_answer = candidate_answers_formatter(candidate_responses_json, focus_key)
-            if focus_answer:
-                tasks.append(HumanMessage(content=(
-                    f'{prompts.TG_CUSTOMER_FOCUS_CRITERIA}\n'
-                    f'{focus_answer}')))
+            with Session() as db:
+                # Забираем все вопросы по вакансии, которые помечены для скрининга
+                screening_questions = db.query(BotQuestion).filter(
+                    BotQuestion.vacancy_id == vacancy_id,
+                    BotQuestion.is_for_screening == True
+                ).all()
+
+                # Если таких нет, то задач для скрининга нет
+                if not screening_questions:
+                    return []
+                
+                # Забираем тексты вопросов, они служат ключами поиска
+                question_texts = [q.question_text for q in screening_questions]
+                # Забираем промпты параметров скрининга для вопросов
+                screening_criterias = [q.screening_criteria for q in screening_questions]
             
-            software_answer = candidate_answers_formatter(candidate_responses_json, software_key)
-            if software_answer:
+            for text, criteria in zip(question_texts, screening_criterias):
                 tasks.append(HumanMessage(content=(
-                    f'{prompts.TG_SOFTWARE_CRITERIA}\n'
-                    f'{software_answer}')))
-            
-            stress_answer = candidate_answers_formatter(candidate_responses_json, stress_key)
-            if stress_answer:
-                tasks.append(HumanMessage(content=(
-                    f'{prompts.TG_STRESS_AT_CALLCENTER_CRITERIA}\n'
-                    f'{stress_answer}'
+                    f'{criteria}\n'
+                    f'{candidate_answers_formatter(candidate_responses_json, [text])}'
                 )))
 
             return tasks
@@ -89,7 +89,8 @@ class TelegramScreening:
     
     async def screen_answers(
         self, 
-        candidate_responses_json: str, 
+        candidate_responses_json: str,
+        vacancy_id: int,
         screening_criterias: str = None
     ) -> str:
         """
@@ -97,13 +98,14 @@ class TelegramScreening:
         
         Args:
             candidate_responses (str): Обобщенные ответы кандидата в JSON формате
+            vacancy_id (int): ID вакансии, по которой пройден опрос
             screening_criterias (str): Требования для скрининга
             
         Returns:
             str: Финальная оценка от GigaChat по кандидату
         """
         # criteria_text = self._format_criteria(screening_criterias) if screening_criterias else ""
-        
+
         # Общая оценка
         global_score = 0
         
@@ -111,21 +113,23 @@ class TelegramScreening:
         system_message = SystemMessage(content=prompts.TELEGRAM_SYSTEM_MESSAGE)
         
         try:
-            tasks = self._collect_tasks(candidate_responses_json)
-            
-            for task_message in tasks:
-                messages = [
-                    system_message,
-                    task_message
-                ]
-                response = await self.giga.ainvoke(messages)
-                task_score = response.content
-                
-                try:
-                    global_score += int(task_score)
-                except (ValueError, TypeError):
-                    pass
-            return global_score
+            tasks = self._collect_tasks(candidate_responses_json, vacancy_id)
+            if len(tasks) > 0:
+                for task_message in tasks:
+                    messages = [
+                        system_message,
+                        task_message
+                    ]
+                    response = await self.giga.ainvoke(messages)
+                    task_score = response.content
+                    
+                    try:
+                        global_score += int(task_score)
+                    except (ValueError, TypeError):
+                        pass
+                return global_score
+            else:
+                return global_score
         
         except Exception as e:
             logger.error(f'Failed to invoke telegram tasks: {tasks}. Error: {str(e)}')
