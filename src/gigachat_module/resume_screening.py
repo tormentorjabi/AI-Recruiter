980c2e10dc5fdd037feb7b97561e10b78ba5b2b5
Maxim Kuznetsov <melixmilan@gmail.com>
2025-05-15 17:45:45 +0500

    feature: modular `resume_screening.py` implementation draft

--- src/gigachat_module/resume_screening.py
+++ src/gigachat_module/resume_screening.py
@@ -1,11 +1,14 @@
 import logging
 import src.gigachat_module.utils.prompts as prompts
 
-from typing import Tuple, List
+from typing import Optional, Any, List
 
 from .client import get_gigachat_client
 from langchain_core.messages import SystemMessage, HumanMessage
 from src.gigachat_module.parser import ResumeData
+
+from src.database.models import ResumeScreeningCriteria, ResumeCriteriaField
+from src.database.session import Session
 
 
 logger = logging.getLogger(__name__)
@@ -28,96 +31,34 @@ class ResumeScreening:
         # Инициализируем GigaChat клиент
         self.giga = get_gigachat_client()
     
-    def _calculate_intermediate_values(self, resume_data: ResumeData) -> Tuple[int, int]:
-        age_score = 0
-        salary_score = 0
-        
-        if resume_data.age:
-                age = resume_data.age
-                if 24 <= age <= 35:
-                    age_score = 20
-                elif 36 <= age <= 42:
-                    age_score = 10
-                elif 18 <= age <= 23:
-                    age_score = 5
-                    
-        if resume_data.salary:
-            salary = resume_data.salary
-            if salary > 60000:
-                salary_score = 0
-            elif 30000 <= salary <= 60000:
-                salary_score = 10
-                
-        return age_score, salary_score
+    async def get_screening_config(self, vacancy_id: int) -> Optional[ResumeScreeningCriteria]:
+        with Session() as db:
+            return db.query(ResumeScreeningCriteria).filter(
+                ResumeScreeningCriteria.vacancy_id == vacancy_id,
+                ResumeScreeningCriteria.is_active == True
+            ).first()
     
-    def _collect_tasks(self, resume_data: ResumeData) -> List[HumanMessage]:
-        tasks = []
-        resume_data_list = resume_data.to_list()
-        try:
-            if resume_data.experiences:
-                # 11 - индекс experience_summary в ResumeData.to_list() списке
-                experience_summary = resume_data_list[11]
-                tasks.append(
-                    HumanMessage(
-                        content=(
-                            f'{prompts.WORK_EXPERIENCE_CRITERIA}\n'
-                            f'{experience_summary}\n'
-                        )
-                    )
-                )
-                if resume_data.skills:
-                    # 10 - индекс skills_summary в ResumeData.to_list() списке
-                    skills_summary =resume_data_list[10]
-                    tasks.append(
-                        HumanMessage(
-                            content=(
-                                f'{prompts.CUSTOMER_FOCUS_CRITERIA}\n'
-                                f'{skills_summary}\n'
-                                f'{experience_summary}\n'
-                        )
-                    ))
-                    
-                    tasks.append(
-                        HumanMessage(
-                            content=(
-                                f'{prompts.COMPUTER_SKILLS_CRITERIA}\n'
-                                f'{skills_summary}\n'
-                                f'{experience_summary}\n'
-                        )
-                    ))
-                    
-                    tasks.append(
-                        HumanMessage(
-                            content=(
-                                f'{prompts.STRESS_RESISTANCE_CRITERIA}\n'
-                                f'{skills_summary}\n'
-                                f'{experience_summary}\n'
-                        )
-                    ))
-                    
-            if resume_data.employment:
-                # 12 - индекс employment_summary в ResumeData.to_list() списке
-                employment_summary = resume_data_list[12]
-                tasks.append(
-                    HumanMessage(
-                        content=(
-                            f'{prompts.SHIFT_WORK_SCHEDULE_EXPERIENCE_CRITERIA}\n'
-                            f'{employment_summary}\n'            
-                    )
-                ))
-            
-            return tasks
-        except Exception as e:
-            logger.error(f'Failed to create tasks from ResumeData: {resume_data}. Error: {str(e)}')  
-            return []
+    def _get_resume_field(self, resume_data: ResumeData, field_name: str) -> Any:
+        """Get specific field from ResumeData"""
+        if field_name == 'skills':
+            return resume_data.skills
+        elif field_name == 'experiences':
+            return resume_data.experiences
+        elif hasattr(resume_data, field_name):
+            return getattr(resume_data, field_name)
+        return None
     
-    async def screen_resume(self, resume_data: ResumeData, job_requirements: str = None) -> int:
+    def _build_prompt(self, template: str, field_value: Any, resume_data: ResumeData) -> str:
+        # TODO: Нужно придумать как собирать данные с ResumeData и объединять их с критерием template
+        pass
+  
+    async def screen_resume(self, resume_data: ResumeData, vacancy_id: int) -> int:
         """
         Провести оценку резюме кандидата с помощью GigaChat
         
         Args:
             resume_data (ResumeData): Обобщенная информация о резюме кандидата
-            job_requirements (str): Специфические требования по вакансии
+            vacancy_id (int): ID вакансии, по которой проводится скрининг
             
         Returns:
             int: Оценка от GigaChat относительно резюме
@@ -128,24 +69,40 @@ class ResumeScreening:
         # Глобальный системный промпт
         system_message = SystemMessage(content=prompts.RESUME_SYSTEM_MESSAGE)
         
+        config = await self.get_screening_config(vacancy_id)
+        if not config:
+            logger.warning(f'No screening config for vacancy: {vacancy_id}')
+            return global_score
+        
         try:
-            age_score, salary_score = self._calculate_intermediate_values(resume_data=resume_data)
-            global_score += age_score + salary_score
-            tasks = self._collect_tasks(resume_data=resume_data)
-            
-            for task_message in tasks:
+            criteria_field_configs: Optional[List[ResumeCriteriaField]] = config.fields
+            for field_config in criteria_field_configs:
+                if not field_config.is_active:
+                    continue
+                
+                field_value = self._get_resume_field(resume_data, field_config.resume_field)
+                if not field_value:
+                    continue
+                
+                prompt = self._build_prompt(
+                    template=field_config.prompt_template,
+                    field_value=field_value,
+                    resume_data=resume_data
+                )
+                
                 messages = [
                     system_message,
-                    task_message
+                    HumanMessage(content=prompt)
                 ]
+                
                 response = await self.giga.ainvoke(messages)
-                task_score = response.content
                 
                 try:
-                    global_score += int(task_score)
+                    score = int(response.content) * field_config.weight
+                    global_score += score
                 except (ValueError, TypeError):
-                    pass
-            return global_score     
+                    logger.error(f'Invalid score response from screening for resume field: {field_config.resume_field}')
+            return global_score  
         except Exception as e:
-            logger.error(f'Failed to invoke tasks: {tasks}. Error: {str(e)}')
+            logger.error(f'Failed to screen resume. Error: {str(e)}')
             return global_score
